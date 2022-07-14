123c73e18cc754ee1f95996f12c8f9639402cb9c
Hardydev <dusgh15378@gmail.com>
2022-07-14 11:19:15 +0900

    refactor : 디렉터링 구조 수정

--- TodayAnbu/Sources/Controllers/ViewController.swift
+++ TodayAnbu/Sources/Controllers/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("sdsdsdsdss")
         // Do any additional setup after loading the view.
     }
 }
