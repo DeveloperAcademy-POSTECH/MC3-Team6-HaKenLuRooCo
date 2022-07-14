5aff84bf19b9ff37cef0d763339fc653ad38e094
luminoux <yungzin98@naver.com>
2022-07-14 10:20:05 +0900

    chore: 프로젝트 초기 세팅

---
+++ TodayAnbu/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TodayAnbu
+//
+//  Created by YeongJin Jeong on 2022/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
