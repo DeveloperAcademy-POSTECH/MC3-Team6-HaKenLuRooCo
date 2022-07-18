7b5bdb257a9bec676e03980f2f98ea7d39cfd9b0
luminoux <yungzin98@naver.com>
2022-07-18 15:12:29 +0900

    [Feat] : Textfield validation & delegate 연결

--- TodayAnbu/Sources/Controllers/SettingViewController.swift
+++ TodayAnbu/Sources/Controllers/SettingViewController.swift
@@ -8,20 +8,59 @@
 import Foundation
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UITextFieldDelegate {
+    var momNumber: String?
+    var dadNumber: String?
+    private var isMomNumberCompleted: Bool = false
 
-    @IBOutlet weak var momNumberTF: UITextField!
-    @IBOutlet weak var dadNumberTF: UITextField!
+    @IBOutlet weak var momNumberTF: UITextField! // 어머니 전화번호 입력 텍스트 필드
+    @IBOutlet weak var dadNumberTF: UITextField! // 아버지 전화번호 입력 텍스트 필드
 
-    override func viewDidLayoutSubviews() {
-        momNumberTF.setBottomBorder(color: UIColor.systemGray4)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        momNumberTF.delegate = self
+        dadNumberTF.delegate = self
+        momNumberTF.setBottomBorder(color: UIColor.systemGray4) // Textfield에 밑줄 추가
         momNumberTF.addDoneButtonOnKeyboard()
         dadNumberTF.setBottomBorder(color: UIColor.systemGray4)
-        dadNumberTF.addDoneButtonOnKeyboard()
+        dadNumberTF.addDoneButtonOnKeyboard() // Keyboard에 done 버튼 추가
     }
-    override func viewDidLoad() {
+    override func viewDidAppear(_ animated: Bool) {
+        if isMomNumberCompleted == true {
+            print("Good")
+        }
+    }
+}
 
-        super.viewDidLoad()
-        print("Setting View Did Loaded")
+extension SettingViewController {
+    //     텍스트 필드 validation check
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+        if textField.text!.count < 10 {
+            textField.setBottomBorder(color: UIColor.red)
+        } else {
+            textField.setBottomBorder(color: UIColor.green)
+        }
+        let validation = textField.text!.count + string.count - range.length
+        return !(validation > 11)
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == momNumberTF {
+            if momNumberTF.text != nil {
+                momNumber = momNumberTF.text!
+                isMomNumberCompleted = true
+                print(momNumber ?? "nil")
+            } else {
+                print("momNumber Error")
+            }
+        } else {
+            if dadNumberTF.text != nil {
+                dadNumber = dadNumberTF.text!
+                print(dadNumber ?? "nil")
+            } else {
+                print("dadNumber Error")
+            }
+        }
     }
 }
--- TodayAnbu/Sources/Extensions/UITextfieldExtension.swift
+++ TodayAnbu/Sources/Extensions/UITextfieldExtension.swift
@@ -10,14 +10,8 @@ import UIKit
 
 extension UITextField {
 
-    public var hasValidEmail: Bool {
-        return text!.range(of: "(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}" +
-                           "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
-                           "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-" +
-                           "z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5" +
-                           "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
-                           "9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
-                           "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
+    public var hasValidPhoneNumber: Bool {
+        return text!.range(of: "^01[0-1, 7][0-9]{7,8}$",
                            options: String.CompareOptions.regularExpression,
                            range: nil, locale: nil) != nil
     }
@@ -46,6 +40,7 @@ extension UITextField {
         }
 
         func addDoneButtonOnKeyboard() {
+            
             let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
             doneToolbar.barStyle = .default
 
