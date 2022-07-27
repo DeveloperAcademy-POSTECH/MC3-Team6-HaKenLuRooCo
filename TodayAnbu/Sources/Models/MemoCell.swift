8f64b671903cc4118cf4a1dac18d8597cb1a432c
Taehwan Kim <obtusa07@gmail.com>
2022-07-27 20:21:06 +0900

    feat: MemoView 개발 & 온보딩 Layout Lint 수정

---
+++ TodayAnbu/Sources/Models/MemoCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemoCell.swift
+//  TodayAnbu
+//
+//  Created by Taehwan Kim on 2022/07/27.
+//
+
+import UIKit
+
+class MemoCell: UICollectionViewCell {
+    func configure(_ memodata: MemoData) {
+        
+    }
+}
