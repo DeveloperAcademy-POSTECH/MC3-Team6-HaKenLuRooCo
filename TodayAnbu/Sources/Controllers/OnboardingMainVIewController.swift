3c9ae45513a629641babf7625e1af56722e3e780
luminoux <yungzin98@naver.com>
2022-07-27 18:06:22 +0900

    [feat] : label 추가

---
+++ TodayAnbu/Sources/Controllers/OnboardingMainVIewController.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingMainVIewController.swift
+//  TodayAnbu
+//
+//  Created by YeongJin Jeong on 2022/07/27.
+//
+
+import Foundation
+import UIKit
+
+class OnboardingMainVIewController: UIViewController {
+
+
+    override func viewDidLoad() {
+
+    }
+
+
+
+
+
+}
---
+++ TodayAnbu/Sources/Controllers/Utilities/OnboardingMainButton.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingMainButton.swift
+//  TodayAnbu
+//
+//  Created by YeongJin Jeong on 2022/07/27.
+//
+
+import UIKit
+
+class OnboardingMainButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .systemGray4
+        
+
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
