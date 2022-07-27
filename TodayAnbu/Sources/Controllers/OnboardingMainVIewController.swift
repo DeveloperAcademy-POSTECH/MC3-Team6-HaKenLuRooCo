e73e5d17eb187b8146118a854d7d61f1acff616c
luminoux <yungzin98@naver.com>
2022-07-27 20:03:01 +0900

    [Feat] : 부모님 선택 UI 구성

--- TodayAnbu/Sources/Controllers/OnboardingMainVIewController.swift
+++ TodayAnbu/Sources/Controllers/OnboardingMainVIewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class OnboardingMainVIewController: UIViewController {
 
+    @IBOutlet var momSettingButton: UIButton!
+    @IBOutlet var dadSettingButton: UIButton!
+
+    private var isMomSelected: Bool = false
+    private var isDadSelected: Bool = false
 
     override func viewDidLoad() {
-
+        momSettingButton.layer.cornerRadius = 5
+        dadSettingButton.layer.cornerRadius = 5
+        momSettingButton.backgroundColor = UIColor.systemGray5
+        dadSettingButton.backgroundColor = UIColor.systemGray5
     }
 
+    // 어머니와 연락 설정하기 버튼 액션 함수
+    // TODO: 추후 로직 수정 필요
+    @IBAction func momSettingButtonAction(_ sender: Any) {
+        isMomSelected.toggle()
+        if isMomSelected {
+            momSettingButton.backgroundColor = UIColor.momDeepPink
+        } else {
+            momSettingButton.backgroundColor = UIColor.systemGray5
+        }
+    }
 
-
-
-
+    // 아버지와 연락 설정하기 버튼 액션 함수
+    // TODO: 추후 로직 수정 필요
+    @IBAction func dadSettingButtonAction(_ sender: Any) {
+        isDadSelected.toggle()
+        if isDadSelected {
+            dadSettingButton.backgroundColor = .dadDeepSkyblue
+        } else {
+            dadSettingButton.backgroundColor = .systemGray5
+        }
+    }
 }
