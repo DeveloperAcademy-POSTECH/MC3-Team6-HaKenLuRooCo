fce0b51544e46be1ec340fa9511947ffc4f19590
luminoux <yungzin98@naver.com>
2022-07-28 22:21:44 +0900

    [Feat] : Label & Asset 추가

---
+++ TodayAnbu/Sources/Controllers/AboutUsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  TodayAnbu
+//
+//  Created by YeongJin Jeong on 2022/07/28.
+//
+
+import Foundation
+import UIKit
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
