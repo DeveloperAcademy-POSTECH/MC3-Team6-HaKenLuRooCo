ba40625cdaff77f3e21f2e83341236f0dc2ff9cd
Hardydev <dusgh15378@gmail.com>
2022-07-28 12:03:04 +0900

    [feat] 테이블 뷰 네비게이트 구현중

--- TodayAnbu/Sources/Controllers/SettingViewController.swift
+++ TodayAnbu/Sources/Controllers/SettingViewController.swift
@@ -10,19 +10,47 @@ import UIKit
 class SettingViewController: UIViewController {
     // MARK: Properties
     let tableView = UITableView(frame: .zero, style: .insetGrouped)
-    let personalMenu = ["요약", "가족 연락처", "알림", "토픽", "About us", "License"]
+    let personalMenu = ["어머니 설정", "아버지 설정", "알림"]
+    let tableViewSection: [String] = ["설정", "추가정보"]
+
+    private let settingTopArea: UIView = {
+        let area = UIView()
+        area.layer.cornerRadius = 20
+        area.backgroundColor = .mainIndigo
+        return area
+    }()
+
+    private let tableCellHeader: UILabel = {
+        let label = UILabel()
+        label.text = "설정"
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.textColor = .systemGray
+        return label
+    }()
+
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewComponent()
     }
+
     func configureViewComponent() {
         view.addSubview(tableView)
+        view.addSubview(settingTopArea)
+        settingTopArea.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(SettingPersonalDataCell.self, forCellReuseIdentifier: "Cell")
+
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            settingTopArea.topAnchor.constraint(equalTo: view.topAnchor),
+            settingTopArea.bottomAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            settingTopArea.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            settingTopArea.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: settingTopArea.bottomAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
             tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
@@ -31,9 +59,6 @@ class SettingViewController: UIViewController {
 }
 
 extension SettingViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return personalMenu.count
-    }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? SettingPersonalDataCell else {
             return UITableViewCell()
@@ -44,17 +69,37 @@ extension SettingViewController: UITableViewDataSource {
         return cell
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personalMenu.count
+    }
+    // Header Height
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60.0
+    }
+
+    // Header Title
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return "설정"
+//    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        tableCellHeader
+    }
 }
 
 extension SettingViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = SettingHeaderView()
-        return header
-    }
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 300
-    }
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let header = SettingHeaderView()
+//        return header
+//    }
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 300
+//    }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+//        let storyboard = UIStoryboard(name: String(describing: UserInitViewController.self), bundle: nil)
+        let userinitViewController = UserInitViewController(nibName: "UserinitVC", bundle: nil)
+//        storyboard.instantiateInitialViewController()!
+        self.navigationController?.pushViewController(userinitViewController, animated: true)
     }
 }
