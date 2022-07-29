2b2e5278695f794a4b6a9141e229e7cafb446f23
luminoux <yungzin98@naver.com>
2022-07-29 11:52:46 +0900

    [Feat]: AboutUsView UI 구성 완료

--- TodayAnbu/Sources/Controllers/AboutUsViewController.swift
+++ TodayAnbu/Sources/Controllers/AboutUsViewController.swift
@@ -9,4 +9,34 @@ import UIKit
 
 class AboutUsViewController: UIViewController {
 
+    @IBOutlet var hardyButton: UIButton!
+    @IBOutlet var kenButton: UIButton!
+    @IBOutlet var lumiButton: UIButton!
+    @IBOutlet var rookieButton: UIButton!
+    @IBOutlet var cozybutton: UIButton!
+
+    @IBOutlet var topTitleBox: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topTitleBox.layer.cornerRadius = 20
+        topTitleBox.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+    }
+
+    @IBAction func hardyButtonAction(_ sender: Any) {
+        
+    }
+
+    @IBAction func kenButtonAction(_ sender: Any) {
+    }
+
+    @IBAction func lumiButtonAction(_ sender: Any) {
+    }
+
+    @IBAction func rookieButtonAction(_ sender: Any) {
+    }
+
+    @IBAction func cozyButtonAction(_ sender: Any) {
+    }
+    
 }
