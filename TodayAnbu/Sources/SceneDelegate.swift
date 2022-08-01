0c0761ee5df2442e1137e9d41519bd5a3f019328
Taekyun Kim <59143479+cozytk@users.noreply.github.com>
2022-08-01 22:34:31 +0900

    Update TodayAnbu/Sources/SceneDelegate.swift

--- TodayAnbu/Sources/SceneDelegate.swift
+++ TodayAnbu/Sources/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-//        setRootViewController(scene)
+        setRootViewController(scene)
         // MARK: 개발 생산성을 위해 엔트리 포인트를 고정하고자 하는 경우 아래의 코드에서 rootViewController를 지정하여 사용
         /*
         guard let scene = (scene as? UIWindowScene) else { return }
