b546594462d085d709f9ffe4402bd553d4832f51
Jay <103009135+Rookie0031@users.noreply.github.com>
2022-08-08 19:52:16 +0900

    feat: memo search 기능 추가

--- TodayAnbu/Sources/Controllers/MemoViewController.swift
+++ TodayAnbu/Sources/Controllers/MemoViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class MemoViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet var collectionView: UICollectionView!
     var list: [MemoData] = []
     typealias Item = MemoData
     enum Section {
@@ -20,6 +21,7 @@ class MemoViewController: UIViewController {
     // var list: [MemoData] = MemoData.list
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         list = makeMemoList()
         collectionView.delegate = self
         // MARK: - presentation
@@ -52,7 +54,7 @@ class MemoViewController: UIViewController {
         groupLayout.interItemSpacing = .fixed(spacing)
         // Section
         let section = NSCollectionLayoutSection(group: groupLayout)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 25, leading: 25, bottom: 0, trailing: 25)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 25, bottom: 0, trailing: 25)
         section.interGroupSpacing = spacing
         return UICollectionViewCompositionalLayout(section: section)
     }
@@ -70,6 +72,7 @@ class MemoViewController: UIViewController {
         }
         return list
     }
+    
 }
 extension MemoViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -78,3 +81,70 @@ extension MemoViewController: UICollectionViewDelegate {
         // navigationController?.pushViewController(SettingViewController(), animated: true)
     }
 }
+
+extension MemoViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            searchBar.delegate = self
+            list = makeMemoList()
+            collectionView.delegate = self
+            dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemoCell", for: indexPath) as? MemoCell else {
+                    return nil
+                }
+                cell.configure(item)
+                cell.layer.cornerRadius = 25
+                cell.layer.shadowOffset = CGSize(width: 5, height: 5)
+                return cell
+            })
+            
+            var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+            snapshot.appendSections([.main])
+            snapshot.appendItems(list, toSection: .main)
+            dataSource.apply(snapshot)
+            collectionView.collectionViewLayout = layout()
+        } else {
+            searchBar.delegate = self
+            list = makeFilteredMemoList()
+            collectionView.delegate = self
+            dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemoCell", for: indexPath) as? MemoCell else {
+                    return nil
+                }
+                cell.configure(item)
+                cell.layer.cornerRadius = 25
+                cell.layer.shadowOffset = CGSize(width: 5, height: 5)
+                return cell
+            })
+            var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+            snapshot.appendSections([.main])
+            snapshot.appendItems(list, toSection: .main)
+            dataSource.apply(snapshot)
+
+            // MARK: - layer
+            collectionView.collectionViewLayout = layout()
+            
+            func makeFilteredMemoList() -> [MemoData] {
+                var list: [MemoData] = []
+                var dict: [String: String] = [:]
+                dict = UserDefaults.standard.value(forKey: "momMemo") as? [String: String] ?? [:]
+                let memoDates = [String](dict.keys)
+                let memoDescription = [String](dict.values).filter({$0.contains(searchText)})
+                let memoCount: Int = memoDescription.count
+                for idx in 0 ..< memoCount {
+                    let strArray = Array(memoDates[idx])
+                    let dateString = "\(strArray[2])\(strArray[3])월 \(strArray[4])\(strArray[5])일"
+                    list.append(MemoData(date: String(dateString), description: memoDescription[idx]))
+                }
+                return list
+            }
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.endEditing(true)
+    }
+}
+
+
