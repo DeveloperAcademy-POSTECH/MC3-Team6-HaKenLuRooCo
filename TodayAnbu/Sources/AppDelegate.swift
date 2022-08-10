0cf55fe131266320ad514ab682cb2c14fa7ccd2d
Rookie0031 <jisu7408@gmail.com>
2022-08-10 14:55:34 +0900

    feat: 런치스크린 수정

--- TodayAnbu/Sources/AppDelegate.swift
+++ TodayAnbu/Sources/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(1)
         return true
     }
 
